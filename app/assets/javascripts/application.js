3aeb3a7749936fd30572932435d87d4e958e3ab7
ZJR <me@zjr.io>
2012-11-28 02:44:04 -0500

    giving foundation a try

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,21 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery_ujs
-//= require foundation
 //= require_tree .
 //= require prefixfree
 //= require html5-bp-plug
+//= require foundation
+
+// Open Sans load
+WebFontConfig = {
+  google: { families: [ 'Open+Sans:600:latin' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
