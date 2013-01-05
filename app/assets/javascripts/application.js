28c4df64cf7f94de62ca66ac66c11d09af11380b
ZJR <me@zjr.io>
2013-01-05 00:29:32 -0500

    working on stuff, meaningful, I know

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,29 @@
 //= require GGS
 //= require_tree ../../../vendor/assets/javascripts/1140gs
 //= require_tree .
+
+$(document).ready(function(){
+
+	$('#m-nav input').click(function() {
+		if($(this).is(':checked')) {
+			switch(this.id) {
+				case "ms-control-1":
+					$('#ms-scroll').removeClass().addClass('pos1')
+					break;
+				case "ms-control-2":
+					$('#ms-scroll').removeClass().addClass('pos2')
+					break;
+				case "ms-control-3":
+					$('#ms-scroll').removeClass().addClass('pos3')
+					break;
+				case "ms-control-4":
+					$('#ms-scroll').removeClass().addClass('pos4')
+					break;
+				case "ms-control-5":
+					$('#ms-scroll').removeClass().addClass('pos5')
+					break;
+			}
+		}
+	});
+
+});
