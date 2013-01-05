31f7d75bf5dae0927438d10ba79535b73b4b2d45
ZJR <me@zjr.io>
2013-01-05 02:47:27 -0500

    testing cap assets handling

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,14 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-  # task :precompile do
-  #   run "bundle exec rake assets:precompile"
-  # end
+  namespace :assets do  
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end    
+    end
+  end
 end
